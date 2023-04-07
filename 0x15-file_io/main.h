7a956e53fe69e4b79f5e32005c8f5f28fda0feb4
CODE12CHAI <chaima.elansali@gmail.com>
2023-04-07 02:36:33 -0700

    0-read_textfile.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,13 +3,14 @@
 
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <elf.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
-int create file(const char *filename, char *text_content);
-int apprend_text_to_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
